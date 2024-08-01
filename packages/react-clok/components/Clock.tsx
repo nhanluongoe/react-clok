8e396807aa21cd1b76bd421641ff1b4b68fb1fc7
nhanluongoe <nhanlt18@gmail.com>
2024-08-01 13:05:12 +0700

    feat: allow to customize clock frame

--- packages/react-clok/components/Clock.tsx
+++ packages/react-clok/components/Clock.tsx
@@ -1,12 +1,11 @@
-import styled from "@emotion/styled";
 import useTime from "../core/use-time";
+import ClockCenter, { ClockCenterProps } from "./ClockCenter";
+import ClockFrame, { ClockFrameProps } from "./ClockFrame";
+import { ClockHandProps } from "./ClockHand";
 import ClockHourHand from "./ClockHourHand";
+import ClockMark, { ClockMarkProps } from "./ClockMark";
 import ClockMinuteHand from "./ClockMinuteHand";
 import ClockSecondHand from "./ClockSecondHand";
-import ClockMark, { ClockMarkProps } from "./ClockMark";
-import { ClockHandProps } from "./ClockHand";
-import ClockFrame, { ClockFrameProps } from "./ClockFrame";
-import ClockCenter, { ClockCenterProps } from "./ClockCenter";
 
 interface ClockProps {
   frame?: ClockFrameProps;
@@ -17,22 +16,18 @@ interface ClockProps {
   mark?: ClockMarkProps;
 }
 
-const ClockWrapper = styled.div``;
-
 export default function Clock(props: ClockProps) {
   const { hourDeg, minuteDeg, secondDeg, iso } = useTime();
   const { frame, hourHand, minuteHand, secondHand, center, mark } = props;
   return (
-    <ClockWrapper>
-      <time dateTime={iso}>
-        <ClockFrame {...frame}>
-          <ClockHourHand deg={hourDeg} {...hourHand} />
-          <ClockMinuteHand deg={minuteDeg} {...minuteHand} />
-          <ClockSecondHand deg={secondDeg} {...secondHand} />
-          <ClockMark {...mark} />
-          <ClockCenter {...center} />
-        </ClockFrame>
-      </time>
-    </ClockWrapper>
+    <time dateTime={iso}>
+      <ClockFrame {...frame}>
+        <ClockHourHand deg={hourDeg} {...hourHand} />
+        <ClockMinuteHand deg={minuteDeg} {...minuteHand} />
+        <ClockSecondHand deg={secondDeg} {...secondHand} />
+        <ClockMark {...mark} />
+        <ClockCenter {...center} />
+      </ClockFrame>
+    </time>
   );
 }
--- packages/react-clok/components/ClockFrame.tsx
+++ packages/react-clok/components/ClockFrame.tsx
@@ -4,21 +4,28 @@ import { CSSProperties, ReactNode } from "react";
 export interface ClockFrameProps {
   children: ReactNode;
   color?: CSSProperties["color"];
-  width?: CSSProperties["borderWidth"];
+  frameWidth?: CSSProperties["borderWidth"];
+  size?: CSSProperties["width"];
+  boxShadow?: CSSProperties["boxShadow"];
 }
 
 const DEFAULT_CONFIG = {
   color: "black",
-  width: "6px",
+  frameWidth: "6px",
+  size: "500px",
+  boxShadow: "0 0 15px rgba(0, 0, 0, 0.3)",
 };
 
 const StyledClockFrame = styled.div<ClockFrameProps>`
-  border-radius: 9999px;
-  border: ${({ width = DEFAULT_CONFIG.width, color = DEFAULT_CONFIG.color }) =>
-    `${width} solid ${color}`};
-  width: 800px;
-  height: 800px;
   position: relative;
+  border-radius: 9999px;
+  border: ${({
+    frameWidth = DEFAULT_CONFIG.frameWidth,
+    color = DEFAULT_CONFIG.color,
+  }) => `${frameWidth} solid ${color}`};
+  width: ${({ size = DEFAULT_CONFIG.size }) => size};
+  height: ${({ size = DEFAULT_CONFIG.size }) => size};
+  box-shadow: ${({ boxShadow = DEFAULT_CONFIG.boxShadow }) => boxShadow}};
 `;
 
 export default function ClockFrame(props: ClockFrameProps) {
