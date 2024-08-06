0490132b7e5461d3dbe2c88f8c74f2df15b80185
nhanluongoe <nhanlt18@gmail.com>
2024-08-06 16:32:01 +0700

    feat: allow to customize segments

--- packages/react-clok/src/components/digital-clock/Clock.tsx
+++ packages/react-clok/src/components/digital-clock/Clock.tsx
@@ -1,25 +1,45 @@
 import useTime from "../../core/use-time";
-import Digit from "./ClockDigit";
+import Digit, { ClockDigitProps } from "./ClockDigit";
 import Separator from "./ClockSeparator";
 import ClockFrame from "./ClockFrame";
+import { CSSProperties } from "react";
 
-export default function DigitalClock() {
+interface DigitalClockProps {
+  hourSegment?: Exclude<ClockDigitProps, "number" | "show">;
+  minuteSegment?: Exclude<ClockDigitProps, "number" | "show">;
+  secondSegment?: Exclude<ClockDigitProps, "number">;
+}
+
+export default function DigitalClock(props: DigitalClockProps) {
+  const { hourSegment, minuteSegment, secondSegment } = props;
+
   const { hour, minute, second, iso } = useTime();
+
+  function renderSecondSegment() {
+    if (secondSegment?.show === false) return null;
+
+    return (
+      <>
+        <Separator />
+
+        <Digit number={Math.floor(second / 10)} {...secondSegment} />
+        <Digit number={second % 10} {...secondSegment} />
+      </>
+    );
+  }
+
   return (
     <time dateTime={iso}>
       <ClockFrame>
-        <Digit number={Math.floor(hour / 10)} />
-        <Digit number={hour % 10} />
+        <Digit number={Math.floor(hour / 10)} {...hourSegment} />
+        <Digit number={hour % 10} {...hourSegment} />
 
         <Separator />
 
-        <Digit number={Math.floor(minute / 10)} />
-        <Digit number={minute % 10} />
+        <Digit number={Math.floor(minute / 10)} {...minuteSegment} />
+        <Digit number={minute % 10} {...minuteSegment} />
 
-        <Separator />
-
-        <Digit number={Math.floor(second / 10)} />
-        <Digit number={second % 10} />
+        {renderSecondSegment()}
       </ClockFrame>
     </time>
   );
--- packages/react-clok/src/components/digital-clock/ClockDigit.tsx
+++ packages/react-clok/src/components/digital-clock/ClockDigit.tsx
@@ -1,194 +1,211 @@
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
+import { CSSProperties } from "react";
 
-const DigitBase = styled.div`
-  border: 1px solid transparent;
-  width: 100px;
-  height: 100px;
-  margin: 0 16px;
-  border-width: 8px;
+const DEFAULT_CONFIG = {
+  width: 16,
+  color: "black",
+  size: 100,
+  gap: 16,
+  show: true,
+};
+
+const DigitBase = styled.div<ClockDigitProps>`
+  border: solid transparent;
+  width: ${({ size = DEFAULT_CONFIG.size }) => size}px;
+  height: ${({ size = DEFAULT_CONFIG.size }) => size}px;
+  margin: 0 ${({ gap = DEFAULT_CONFIG.gap }) => gap}px;
+  border-width: ${({ width = DEFAULT_CONFIG.width }) => width}px;
+  color: ${({ color = DEFAULT_CONFIG.color }) => color};
 `;
 
-function topFragmentStyle(props: { num: number }) {
-  const { num } = props;
-  if (num === 0) {
-    return css`
-      border-top-color: black;
-      border-right-color: black;
-      border-left-color: black;
-    `;
-  }
-
-  if (num === 1) {
-    return css`
-      border-right-color: black;
-    `;
-  }
-
-  if (num === 2) {
-    return css`
-      border-top-color: black;
-      border-right-color: black;
-      border-bottom-color: black;
-    `;
-  }
-
-  if (num === 3) {
-    return css`
-      border-top-color: black;
-      border-right-color: black;
-      border-bottom-color: black;
-    `;
-  }
-
-  if (num === 4) {
-    return css`
-      border-right-color: black;
-      border-left-color: black;
-      border-bottom-color: black;
-    `;
-  }
-
-  if (num === 5) {
-    return css`
-      border-top-color: black;
-      border-left-color: black;
-      border-bottom-color: black;
-    `;
-  }
-
-  if (num === 6) {
-    return css`
-      border-top-color: black;
-      border-left-color: black;
-      border-bottom-color: black;
-    `;
-  }
-
-  if (num === 7) {
-    return css`
-      border-top-color: black;
-      border-right-color: black;
-      border-bottom-color: black;
-    `;
-  }
-
-  if (num === 8) {
-    return css`
-      border-top-color: black;
-      border-right-color: black;
-      border-left-color: black;
-      border-bottom-color: black;
-    `;
-  }
-
-  if (num === 9) {
-    return css`
-      border-top-color: black;
-      border-right-color: black;
-      border-left-color: black;
-      border-bottom-color: black;
-    `;
-  }
-}
-
-function bottomFragmentStyle(props: { num: number }) {
-  const { num } = props;
-
-  if (num === 0) {
-    return css`
-      border-right-color: black;
-      border-left-color: black;
-      border-bottom-color: black;
-    `;
-  }
-
-  if (num === 1) {
-    return css`
-      border-right-color: black;
-    `;
-  }
-
-  if (num === 2) {
-    return css`
-      border-top-color: black;
-      border-bottom-color: black;
-      border-left-color: black;
-    `;
-  }
-
-  if (num === 3) {
-    return css`
-      border-right-color: black;
-      border-top-color: black;
-      border-bottom-color: black;
-    `;
-  }
-
-  if (num === 4) {
-    return css`
-      border-right-color: black;
-      border-top-color: black;
-    `;
-  }
-
-  if (num === 5) {
-    return css`
-      border-top-color: black;
-      border-right-color: black;
-      border-bottom-color: black;
-    `;
-  }
-
-  if (num === 6) {
-    return css`
-      border-top-color: black;
-      border-left-color: black;
-      border-bottom-color: black;
-      border-right-color: black;
-    `;
-  }
-
-  if (num === 7) {
-    return css`
-      border-right-color: black;
-      border-top-color: black;
-    `;
-  }
-
-  if (num === 8) {
-    return css`
-      border-top-color: black;
-      border-right-color: black;
-      border-left-color: black;
-      border-bottom-color: black;
-    `;
-  }
-
-  if (num === 9) {
-    return css`
-      border-right-color: black;
-      border-top-color: black;
-      border-bottom-color: black;
-    `;
-  }
-}
-
-const DigitTop = styled(DigitBase)<{ num: number }>`
-  border-bottom-width: 4px;
+const DigitTop = styled(DigitBase)<ClockDigitProps>`
+  border-bottom-width: ${({ width = DEFAULT_CONFIG.width }) => width / 2}px;
   ${topFragmentStyle};
 `;
 
-const DigitBottom = styled(DigitBase)<{ num: number }>`
-  border-top-width: 4px;
+const DigitBottom = styled(DigitBase)<ClockDigitProps>`
+  border-top-width: ${({ width = DEFAULT_CONFIG.width }) => width / 2}px;
   ${bottomFragmentStyle};
 `;
 
-export default function ClockDigit({ number }: { number: number }) {
+export interface ClockDigitProps {
+  number: number;
+  width?: number;
+  color?: CSSProperties["color"];
+  show?: boolean;
+  size?: number;
+  gap?: number;
+}
+
+export default function ClockDigit(props: ClockDigitProps) {
   return (
     <div>
-      <DigitTop num={number}></DigitTop>
-      <DigitBottom num={number}></DigitBottom>
+      <DigitTop {...props}></DigitTop>
+      <DigitBottom {...props}></DigitBottom>
     </div>
   );
+}
+
+function topFragmentStyle(props: { number: number }) {
+  const { number } = props;
+  if (number === 0) {
+    return css`
+      border-top-color: black;
+      border-right-color: black;
+      border-left-color: black;
+    `;
+  }
+
+  if (number === 1) {
+    return css`
+      border-right-color: black;
+    `;
+  }
+
+  if (number === 2) {
+    return css`
+      border-top-color: black;
+      border-right-color: black;
+      border-bottom-color: black;
+    `;
+  }
+
+  if (number === 3) {
+    return css`
+      border-top-color: black;
+      border-right-color: black;
+      border-bottom-color: black;
+    `;
+  }
+
+  if (number === 4) {
+    return css`
+      border-right-color: black;
+      border-left-color: black;
+      border-bottom-color: black;
+    `;
+  }
+
+  if (number === 5) {
+    return css`
+      border-top-color: black;
+      border-left-color: black;
+      border-bottom-color: black;
+    `;
+  }
+
+  if (number === 6) {
+    return css`
+      border-top-color: black;
+      border-left-color: black;
+      border-bottom-color: black;
+    `;
+  }
+
+  if (number === 7) {
+    return css`
+      border-top-color: black;
+      border-right-color: black;
+    `;
+  }
+
+  if (number === 8) {
+    return css`
+      border-top-color: black;
+      border-right-color: black;
+      border-left-color: black;
+      border-bottom-color: black;
+    `;
+  }
+
+  if (number === 9) {
+    return css`
+      border-top-color: black;
+      border-right-color: black;
+      border-left-color: black;
+      border-bottom-color: black;
+    `;
+  }
+}
+
+function bottomFragmentStyle(props: { number: number }) {
+  const { number } = props;
+
+  if (number === 0) {
+    return css`
+      border-right-color: black;
+      border-left-color: black;
+      border-bottom-color: black;
+    `;
+  }
+
+  if (number === 1) {
+    return css`
+      border-right-color: black;
+    `;
+  }
+
+  if (number === 2) {
+    return css`
+      border-top-color: black;
+      border-bottom-color: black;
+      border-left-color: black;
+    `;
+  }
+
+  if (number === 3) {
+    return css`
+      border-right-color: black;
+      border-top-color: black;
+      border-bottom-color: black;
+    `;
+  }
+
+  if (number === 4) {
+    return css`
+      border-right-color: black;
+      border-top-color: black;
+    `;
+  }
+
+  if (number === 5) {
+    return css`
+      border-top-color: black;
+      border-right-color: black;
+      border-bottom-color: black;
+    `;
+  }
+
+  if (number === 6) {
+    return css`
+      border-top-color: black;
+      border-left-color: black;
+      border-bottom-color: black;
+      border-right-color: black;
+    `;
+  }
+
+  if (number === 7) {
+    return css`
+      border-right-color: black;
+    `;
+  }
+
+  if (number === 8) {
+    return css`
+      border-top-color: black;
+      border-right-color: black;
+      border-left-color: black;
+      border-bottom-color: black;
+    `;
+  }
+
+  if (number === 9) {
+    return css`
+      border-right-color: black;
+      border-top-color: black;
+      border-bottom-color: black;
+    `;
+  }
 }
