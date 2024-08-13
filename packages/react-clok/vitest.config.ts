da7f24e3026f9b91c53757e1be445430a8fe13b3
nhanluongoe <nhanlt18@gmail.com>
2024-08-13 09:43:13 +0700

    chore: add test:watch mode

--- packages/react-clok/vitest.config.ts
+++ packages/react-clok/vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
+    watch: false,
     environment: "jsdom",
     setupFiles: "./vitest.setup.ts",
     include: ["./src/**/*.test.tsx"],
