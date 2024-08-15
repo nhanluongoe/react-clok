dcb93c29e775b914c033f63645923db948cc9c5f
nhanluongoe <nhanlt18@gmail.com>
2024-08-15 15:25:50 +0700

    chore: update prop name

--- packages/react-clok/src/components/analog-clock/ClockCenter.tsx
+++ packages/react-clok/src/components/analog-clock/ClockCenter.tsx
@@ -3,19 +3,19 @@ import { CSSProperties } from "react";
 
 export interface ClockCenterProps {
   color?: CSSProperties["color"];
-  radius?: CSSProperties["width"];
+  size?: CSSProperties["width"];
 }
 
 const DEFAULT_CONFIG = {
   color: "black",
-  radius: "5%",
+  size: "5%",
 };
 
 const StyledClockCenter = styled.div<ClockCenterProps>`
   position: absolute;
-  width: ${({ radius = DEFAULT_CONFIG.radius }) => radius};
-  height: ${({ radius = DEFAULT_CONFIG.radius }) => radius};
-  background-color: #000;
+  width: ${({ size = DEFAULT_CONFIG.size }) => size};
+  height: ${({ size = DEFAULT_CONFIG.size }) => size};
+  background-color: ${({ color = DEFAULT_CONFIG.color }) => color};
   border-radius: 9999px;
   top: 50%;
   left: 50%;
